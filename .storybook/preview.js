2e3016674a0c1e6cbb4178d13943295874937d2c
John Beresford <jberesford@volcaus.com>
2022-12-31 17:58:58 -0800

    chore: hide storybook panel attempt 1000

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,1 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  options: {
-    showPanel: false,
-  },
-};
+export const parameters = {};
